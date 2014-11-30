ec69823f14345780ff1fc67fc30230e0c098c412
Daryl <dkranec@gmail.com>
2014-11-29 22:05:32 -0500

    changed pixel path to have unified data dir

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var pixel = fs.readFileSync(__dirname + '/../pixel.gif');
+var pixel = fs.readFileSync(__dirname + '/../data/pixel.gif');
 
 /* GET home page. */
 router.get('/', function(req, res) {
