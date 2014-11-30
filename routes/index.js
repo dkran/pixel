5393ec4ca14f14b7d98a5a801c7b53bfc05fbf8f
Daryl <dkranec@gmail.com>
2014-11-29 23:09:26 -0500

    geoip changes

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,29 @@ router.get('/', function(req, res) {
 });
 
 router.get('/r/:img', function(req, res) {
+	var reqData = {};
 	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 	var imgId = req.param('img');
 	imgId = imgId.substr(0,(imgId.length-4));
-	console.log(req.headers);
-	console.log(cities.getGeoDataSync(ip));
+	cities.getGeoData(ip, function(err,geodata){
+		if(err){
+			console.log(err);
+		}
+		else{
+			reqData = {
+				'id': imgId,
+				'referer': req.headers['referer'],
+				'user-agent': req.headers['user-agent'],
+				'city': geodata.city.names.en,
+				'state': geodata.subdivisions.names.en,
+				'state_iso': geodata.subdivisions.iso_code,
+				'country': geodata.country.names.en,
+				'country_iso': geodata.country.iso_code 
+			};
+			console.log(reqData);
+		}
+		});
+
 	res.type('image/gif');
 	res.end(pixel, 'binary');
 });
