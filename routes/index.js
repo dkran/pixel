494d03498bc5bd0d3d5f37c322ec2249dcee13b0
Daryl <dkranec@gmail.com>
2014-11-29 21:11:46 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var pixel = fs.readFileSync(__dirname + '/../pixel.gif');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/r/:img', function(req, res) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	var imgId = req.param('img');
+	imgId = imgId.substr(0,(imgId.length-4));
+	console.log(req.headers);
+	console.log(ip);
+	res.type('image/gif');
+	res.end(pixel, 'binary');
+});
+
+
+
+module.exports = router;
