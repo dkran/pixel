4e1dc54497a0a5b98fb2930e078a34fe02f72a05
Daryl Kranec <dkranec@gmail.com>
2014-12-04 21:44:07 +0000

    removed default users route from express generator.  Why does it do that?

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -14,7 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('port', 4000);
-app.set('trust proxt', true);
+app.set('trust proxy', true);
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
