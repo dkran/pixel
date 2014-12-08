49bb0e37068363743b5ea5995b8939b01303b66e
Daryl <dkranec@gmail.com>
2014-12-07 21:59:27 -0500

    updated sqlite db for test interface.

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
+var tracker = require('./lib/pixel-track')
+var indexRoute = require('./routes/index');
 var api = require('./routes/api/index');
 
 var app = express();
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/:img', tracker.requestHandler);
 app.use('/api', api);
 
 /// catch 404 and forward to error handler
