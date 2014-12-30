41ba312088a1c6e8df9f539320390fe1f08a2d0d
Daryl <dkranec@gmail.com>
2014-12-30 13:01:42 -0500

    view all route

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,20 +4,11 @@ var BodyParser = require('body-parser');
 var jsonParse = BodyParser.json();
 
 app.get(/^\/r\/([\w._-]+.gif)$/i, tracker.requestHandler);
-
-app.post('/new', function(req, res){
-	//req.body.userid = the user id.  etc.
-
-	res.status(201).json({
-		'id': 123,
-		'location': '/r/123.gif'
-	}).end()
-});
-
-
 app.get('/users', tracker.getUsers)
 app.get('/users/:id', tracker.getUsers)
 app.get('/clear/:id', tracker.clearVisits)
+app.get('/v/:id', tracker.listVisits)
+app.get('/v/',tracker.listVisits)
 app.post('/users/new', jsonParse, tracker.newUser)
 
 
